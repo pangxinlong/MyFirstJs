d64b75a0dde01801044a59dd8a1698b98e0ae0e1
pangxinlong <670116586@qq.com>
2014-01-19 23:21:28 +0800

    a

---
+++ HelloWorld.js
@@ -0,0 +1,9 @@
+http = require("http");  
+http.createServer(function(request, response) {  
+    response.writeHead(200, {"Content-Type": "text/html"});  
+    response.write("pangxinlong!!!!!!@#$%");  
+    response.end();  
+}).listen(8083);
+var sys = require("sys");
+sys.puts("My first Node.js code."); 
+sys.puts("Server running at http://localhost:8083/"); 
