5140add6b29e3fa5cd70c8d99b0f288d72a4977d
pangxinlong <670116586@qq.com>
2014-01-19 05:32:13 -0800

    d

--- HelloWord.js
+++ HelloWord.js
@@ -1,7 +1,7 @@
 http = require("http");  
 http.createServer(function(request, response) {  
     response.writeHead(200, {"Content-Type": "text/html"});  
-    response.write("!!!!!!!!!!!!!!!!!");  
+    response.write("pangxinlong");  
     response.end();  
 }).listen(8083);
 var sys = require("sys");
